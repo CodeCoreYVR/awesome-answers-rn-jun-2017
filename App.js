165cbc6c83ef2a55262b12772757b29091619a29
Steve Godin <steve.god@gmail.com>
2017-08-18 11:14:01 -0700

    Add requests and QuestionsIndexScreen

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import QuestionsIndexScreen from './src/components/screens/QuestionsIndexScreen';
+import { Question } from './src/utilities/requests';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <QuestionsIndexScreen />
       </View>
     );
   }
